abc1fe4860889e9a794b375d6393e611f8807465
vankoosh <vankoosh@gmail.com>
2022-12-07 19:19:25 +0000

    skeleton started

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
+import Menu from './modules/Left-Menu/Menu.jsx';
+import Sidebar from './modules/Right-Sidebar/Sidebar.jsx';
+// import Tweets from './modules/Tweets/Tweets.jsx';
 
 function App() {
   return (
     <div className="App">
-      
+      <Menu />
+      <Sidebar />
     </div>
   );
 }
---
+++ src/modules/Left-Menu/Menu.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import "./Menu.css"
+
+export default function Menu() {
+  return (
+    <nav>
+      <ul>
+        <li>BIRD LOGO</li>
+        <li>Home</li>
+        <li>Explore</li>
+        <li>Notifications</li>
+        <li>Messages</li>
+        <li>Bookmarks</li>
+        <li>Lists</li>
+        <li>Profile</li>
+        <li>More</li>
+        <li>Tweet</li>
+    </ul>
+    </nav>
+)
+}
