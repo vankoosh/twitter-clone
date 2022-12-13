1d95ccea26d0116f904d1278503c9756dc95d273
vankoosh <vankoosh@gmail.com>
2022-12-13 17:57:50 +0000

    before applying context provider

---
+++ src/global.js
@@ -0,0 +1,6 @@
+const globalCSS = {
+  color_white: "#f7f9f9",
+  color_black: "#000"
+};
+
+export default globalCSS;
--- src/modules/Left-Menu/Menu.jsx
+++ src/modules/Left-Menu/Menu.jsx
@@ -1,22 +1,31 @@
-import React from 'react';
-import "./Menu.css"
+import React from "react";
+import { IconContext } from "react-icons";
+import { VscBell } from "react-icons/vsc";
+import "./Menu.css";
+import globalCSS from "../../global.js"
+
 
 export default function Menu() {
   return (
-    <nav className='nav'>
+    <nav className="nav">
       <ul>
-        <li>BIRD LOGO</li>
-        <li>Home</li>
-        <li>Explore</li>
-        <li>Notifications</li>
-        <li>Messages</li>
-        <li>Bookmarks</li>
-        <li>Lists</li>
-        <li>Profile</li>
-        <li>More</li>
-        <li>Tweet</li>
-        <li>Account</li>
-    </ul>
+        <IconContext.Provider value={{ size: "4rem", color: globalCSS.color_white }}>
+          <li>BIRD LOGO</li>
+          <li>Home</li>
+          <li>Explore</li>
+          <li>
+            <VscBell />
+            Notifications
+          </li>
+          <li>Messages</li>
+          <li>Bookmarks</li>
+          <li>Lists</li>
+          <li>Profile</li>
+          <li>More</li>
+          <li>Tweet</li>
+          <li>Account</li>
+        </IconContext.Provider>
+      </ul>
     </nav>
-)
+  );
 }
